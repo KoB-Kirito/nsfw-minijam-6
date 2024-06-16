8086bfc72bdd2e69b75d5d6f34af936fd79e1cf0
TheWhiteShadow <TheWhiteShadow87@gmail.com>
2024-06-16 23:35:22 +0200

    turmskript

---
+++ game/levels/turm.gd
@@ -0,0 +1,25 @@
+extends Node3D
+
+@export var startX : float = 10
+@export var maxAngle : float = 180
+@export var radius : float = 10
+@export var player : Node3D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var endX = startX + radius * 2 * maxAngle / 360
+	if (player.global_position.x <= startX):
+		global_position.x = startX
+		global_rotation.y = 0
+		return
+	if (player.global_position.x >= endX):
+		global_position.x = endX
+		global_rotation_degrees.y = maxAngle
+		return
+	global_position.x = player.global_position.x
+	global_rotation.y = (player.global_position.x - startX) / radius * -PI
