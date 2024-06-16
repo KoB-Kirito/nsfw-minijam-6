c3f53811d4c7a20b491d41edd36662eb1e3b009a
Kirito <kilito@pm.me>
2024-06-16 03:01:44 +0200

    refactor player script, fix node-path bug on main character

--- game/player/player.gd
+++ game/player/player.gd
@@ -1,85 +1,71 @@
 extends CharacterBody3D
 
 
-@export var SPEED : float = 5
-@export var SMOOTH_SPEED: float = 10
-@export var ROTATION_SPEED: float = 5
-@export var FALL_DECAY: float = 100
-@export var CAMERA_FORWARD_DIST = 2
-@export var CAMERA_FORWARD_SPEED = 0.5
-const JUMP_VELOCITY = 4.5
+@export_group("Movement")
+@export var speed : float = 5.0
+@export var smooth_speed: float = 10.0
+@export var rotation_speed: float = 5.0
 
-@onready var model: Node3D = $Model
-@onready var cameraTaget: Node3D = $CameraTarget
-@onready var animTree: AnimationTree = find_child("AnimationTree")
-@onready var animPlayer: AnimationPlayer = find_child("AnimationPlayer")
+@export_group("Jump")
+@export var jump_velocity: float = 4.5
+@export var gravity: float = 9.8
+## What the fuck does this do?
+@export var fall_decay: float = 2.0
 
-var _active: bool = true
-var direction: Vector3 = Vector3.ZERO
-var lastDirection: Vector3 = Vector3.RIGHT
-var cameraTargetOffset: Vector3 = Vector3(0, 2.25, 0)
-var cameraCurrentDist: float = 0
+@export_group("Camera")
+@export var camera_forward_distance: float = 2.0
+@export var camera_forward_speed: float = 0.5
+@export var camera_target_offset: Vector3 = Vector3(0, 2.25, 0)
 
-var _footstepSound: AudioStream = preload("res://assets/audio/sounds/steps_floor.ogg")
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+var last_direction: Vector3 = Vector3.RIGHT
+var camera_current_distance: float = 0
+
+
+#HACK
+@onready var animation_tree: AnimationTree = find_child("AnimationTree")
+
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
-		if Input.is_action_pressed("up"):
+		if Input.is_action_pressed("jump"):
 			velocity.y -= gravity * delta
 		else:
 			velocity.y -= gravity * delta * 2
 	else:
 		velocity.y = 0
-
+	
 	# Handle Jump.
-	if _active and Input.is_action_just_pressed("up") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	if _active:
-		# Get the input direction and handle the movement/deceleration.
-		# As good practice, you should replace UI actions with custom gameplay actions.
-		var input_dir = Input.get_vector("backward", "forward", "down", "up")
-		direction = (transform.basis * Vector3(input_dir.x, 0, 0)).normalized()
-		
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_velocity
+	
+	
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("left", "right", "crouch", "jump")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, 0)).normalized()
 	
 	if is_on_floor(): # don't walk while airborn
-		velocity.x = lerp(velocity.x, direction.x * SPEED, SMOOTH_SPEED * delta)
-		velocity.z = lerp(velocity.z, direction.z * SPEED, SMOOTH_SPEED * delta)
-
+		velocity.x = lerp(velocity.x, direction.x * speed, smooth_speed * delta)
+		velocity.z = lerp(velocity.z, direction.z * speed, smooth_speed * delta)
+		
 		# set animation blend based on speed
-		animTree.set("parameters/IdleToRun/blend_position", velocity.length() / SPEED)
+		animation_tree.set("parameters/IdleToRun/blend_position", velocity.length() / speed)
 	else:
-		velocity.x = lerp(velocity.x, direction.x * SPEED, SMOOTH_SPEED * delta / FALL_DECAY)
-		velocity.z = lerp(velocity.z, direction.z * SPEED, SMOOTH_SPEED * delta / FALL_DECAY)
+		velocity.x = lerp(velocity.x, direction.x * speed, smooth_speed * delta / fall_decay)
+		velocity.z = lerp(velocity.z, direction.z * speed, smooth_speed * delta / fall_decay)
 		# TODO: falling???
-		animTree.set("parameters/IdleToRun/blend_position", 0)
-
-
+		animation_tree.set("parameters/IdleToRun/blend_position", 0)
+	
 	move_and_slide()
-
+	
+	
 	# save last move direction
 	if direction:
-		cameraCurrentDist = lerp(cameraCurrentDist, direction.x * CAMERA_FORWARD_DIST, SMOOTH_SPEED * delta * CAMERA_FORWARD_SPEED)
-		cameraTaget.global_position = global_position + cameraTargetOffset + Vector3.RIGHT * cameraCurrentDist
-		lastDirection = direction
+		last_direction = direction
+		camera_current_distance = lerp(camera_current_distance, direction.x * camera_forward_distance, smooth_speed * delta * camera_forward_speed)
+		%CameraTarget.global_position = global_position + camera_target_offset + Vector3.RIGHT * camera_current_distance
 	
 	# rotate player to face movement direction
-	model.rotation.y = lerp_angle(model.rotation.y, atan2(lastDirection.x, lastDirection.z), ROTATION_SPEED * delta)
-
-
-func _on_console_activated():
-	_active = false
-
-
-func _on_console_deactivated():
-	_active = true
-
-
-#func _on_area_3d_body_entered(_body: Node3D) -> void:
-	#TODO: Sound
-	#var asPlayer = SoundManager.play_sound(_footstepSound, "SFX")
-	#asPlayer.pitch_scale = random_pitch
+	%Model.rotation.y = lerp_angle(%Model.rotation.y, atan2(last_direction.x, last_direction.z), rotation_speed * delta)
