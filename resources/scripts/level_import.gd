6b26322be6ad0f82940086caaee4edef20e00f73
Kirito <kilito@pm.me>
2024-07-01 11:09:59 +0200

    refactor

--- resources/scripts/level_import.gd
+++ resources/scripts/level_import.gd
@@ -1,52 +1,80 @@
 @tool # Needed so it runs in editor.
 extends EditorScenePostImport
 
-var scene
 
-func _post_import(scene):
-	self.scene = scene
-	var file = get_source_file()
+var _scene: Node
+
+
+func _post_import(scene: Node) -> Node:
+	_scene = scene
+	
+	var file := get_source_file()
 	print_rich("Importiere: [b]%s[/b]" % [file])
+	
+	# recursive iteration
 	iterate(scene)
+	
 	return scene
 
-func iterate(node):
-	if node != null:
-		if node.name.ends_with("-colbox") and node is MeshInstance3D:
+
+func iterate(node: Node) -> void:
+	if node == null:
+		return
+	
+	if node is MeshInstance3D:
+		if node.name.ends_with("-colbox"):
 			handle_box_collider(node)
-		else:
-			if node is MeshInstance3D:
-				map_materials(node)
-			for child in node.get_children():
-				iterate(child)
+			return
+		
+		map_materials(node)
+	
+	for child in node.get_children():
+		iterate(child)
 
-func map_materials(node: MeshInstance3D):
-	for i in range(node.mesh.get_surface_count()):
-		var mat = node.mesh.surface_get_material(i)
-		print_rich("Material: %s" % [mat.resource_name])
 
-func handle_box_collider(node: MeshInstance3D):
+func handle_box_collider(node: MeshInstance3D) -> void:
 	print_rich("Custom Collider: [b]%s[/b]" % [node.name])
 	
-	var aabb = (node as MeshInstance3D).mesh.get_aabb()
-	var box_pos = aabb.get_center()
-	var box_size = aabb.size
-	var collider = CollisionShape3D.new()
+	var aabb := node.mesh.get_aabb()
+	var box_pos := aabb.get_center()
+	var box_size := aabb.size
+	
+	# generate static body
+	var body := StaticBody3D.new()
+	body.set_name("StaticBody3D")
+	body.position = box_pos
+	
+	# generate collider
+	var collider := CollisionShape3D.new()
 	collider.set_name("BoxCollider")
 	collider.shape = BoxShape3D.new()
 	collider.shape.size = box_size
 	
-	var body = StaticBody3D.new()
-	body.set_name("StaticBody3D")
-	body.position = box_pos
-	
-	var parent = node.get_parent()
+	# add body and collider to scene
+	var parent := node.get_parent()
 	add_child(parent, body)
 	add_child(body, collider)
-	parent.remove_child(node)
-	node.owner = null
+	
+	# remove old mesh instance
+	node.queue_free()
+
+
+func map_materials(node: MeshInstance3D) -> void:
+	for i in node.mesh.get_surface_count():
+		var mat = node.mesh.surface_get_material(i)
+		print_rich("Material: %s" % [mat.resource_name])
+		
+		# check if file exists
+		FileAccess.file_exists("path")
+		
+		# load material from file
+		ResourceLoader.load("path")
+		
+		# apply material
+		#node.mesh.surface_set_material()
+
 
 # Reihenfolge ist wichtig, immer von oben nach unten
-func add_child(parent, node):
+func add_child(parent: Node, node: Node) -> void:
 	parent.add_child(node)
-	node.owner = self.scene
+	node.owner = _scene
